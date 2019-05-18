b482d2011d5ab887d403cc88ec316033f3b20616
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2019-05-18 14:31:32 +0300

    Start is good

--- DesidnMaterialsExperements/MainWindow.xaml.cs
+++ DesidnMaterialsExperements/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,20 @@ namespace DesidnMaterialsExperements
         public MainWindow()
         {
             InitializeComponent();
+            Mess.Blocks.Clear();
+            Card card = new Card();
+            card.Padding = new Thickness { Bottom = 8, Left = 8, Right = 8, Top = 8 };
+            TextBlock textBlock = new TextBlock();
+            //textBlock.Style = 
+            textBlock.Text = "bla bla bla";
+            textBlock.FontSize = 14;
+            card.Content = textBlock;
+            Paragraph paragraph = new Paragraph();
+            paragraph.Inlines.Add(card);
+            Mess.Blocks.Add(paragraph);
+
         }
+
+        
     }
 }
